c697c79fcdabccc93115de8fb5a067e54803d264
Nikita <pozhidaevw@gmail.com>
2023-10-13 17:33:55 +0700

    feat: new global styles, remove test content and style

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import styles from "./page.module.scss";
-import { RiGithubLine } from "react-icons/ri";
 
 export default function Home() {
-  return (
-    <div>
-      <RiGithubLine className={styles.icons} />
-    </div>
-  );
+  return <div></div>;
 }
