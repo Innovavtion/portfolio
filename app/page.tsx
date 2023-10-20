c380f16be8d58e5352d83af0a7b9f7b98d69de95
Nikita <pozhidaevw@gmail.com>
2023-10-20 20:17:26 +0700

    fix: structur site

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.scss";
+import Home from "@/components/Home/Home";
 import About from "@/components/About/About";
 import Stack from "@/components/Stack/Stack";
 
-export default function Home() {
+export default function Page() {
   return (
     <>
+      <Home />
       <About />
       <Stack />
     </>
--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,77 +1,10 @@
-"use client";
-
-import { Button } from "../Base/Button/Button";
 import styles from "./About.module.scss";
-import ReactTyped from "react-typed";
-import {
-  SiTypescript,
-  SiReact,
-  SiTailwindcss,
-  SiGithub,
-  SiTelegram,
-  SiGmail,
-} from "react-icons/si";
-import { IoLogoSass, IoIosMore } from "react-icons/io";
-import { FiDownload } from "react-icons/fi";
-import Image from "next/image";
-import LaptopSvg from "@/public/Images/Laptop.svg";
 
 export default function About() {
   return (
     <div className={styles.About}>
       <div className={styles.Container}>
-        <div className={styles.ContainerLaptop}>
-          <div className={styles.LaptopContent}>
-            <div className={styles.Content}>
-              <p className={styles.TextFIO}>Nikita Pozhidaev</p>
-              <p className={styles.TextTypes} translate="no">
-                <ReactTyped
-                  strings={["Web Developer", "React Developer"]}
-                  typeSpeed={150}
-                  loop
-                  backSpeed={35}
-                  cursorChar="❚"
-                  showCursor={true}
-                />
-              </p>
-              <div className={styles.Stack}>
-                <Button>
-                  <SiTypescript className={styles.TypeScript} />
-                </Button>
-                <Button>
-                  <SiReact className={styles.React} />
-                </Button>
-                <Button>
-                  <IoLogoSass className={styles.Sass} />
-                </Button>
-                <Button>
-                  <SiTailwindcss className={styles.Tailwind} />
-                </Button>
-                <Button>
-                  <IoIosMore />
-                </Button>
-              </div>
-              <div className={styles.Contact}>
-                <Button>
-                  <SiGmail className={styles.GMail} />
-                </Button>
-                <Button>
-                  <SiGithub className={styles.GitHub} />
-                </Button>
-                <Button>
-                  <SiTelegram className={styles.Telegram} />
-                </Button>
-              </div>
-              <div className={styles.DownloadResume}>
-                <Button>
-                  <FiDownload className={styles.DownloadIcon} />
-                  <p>Resume</p>
-                </Button>
-              </div>
-            </div>
-          </div>
-          <Image src={LaptopSvg} alt="Laptop" className={styles.Laptop} />
-        </div>
+        <h2>About</h2>
       </div>
     </div>
   );
