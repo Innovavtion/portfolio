bc99ee21e79b2a1b9d4c0c334d19c2723bc00a30
Nikita <pozhidaevw@gmail.com>
2023-10-28 15:33:03 +0700

    feat: create portfolio - data & logic

---
+++ components/Section/Portfolio/Portfolio.data.tsx
@@ -0,0 +1,82 @@
+import { StaticImageData } from "next/image";
+
+import KanbanImage from "@/public/Images/Kanban.png";
+import WeatherImage from "@/public/Images/Weather.png";
+import ToDoImage from "@/public/Images/ToDo.png";
+
+import {
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiMui,
+} from "react-icons/si";
+
+import { IoLogoSass } from "react-icons/io";
+
+interface LinkProps {
+  github: string;
+  vercel: string;
+}
+
+interface StackProps {
+  name: string;
+  icon: React.ReactNode;
+}
+
+interface portfolioProps {
+  name: string;
+  description: string;
+  stack: Array<StackProps>;
+  image: StaticImageData;
+  link: LinkProps;
+}
+
+export const portfolioData: Array<portfolioProps> = [
+  {
+    name: "Kanban",
+    description:
+      "Приложение для управления развитием проектов, который помогает наглядно представлять задачи и добиться максимальной эффективности разработки.",
+    stack: [
+      { name: "TypeScript", icon: <SiTypescript /> },
+      { name: "Next", icon: <SiNextdotjs /> },
+      { name: "Mui", icon: <SiMui /> },
+    ],
+    image: KanbanImage,
+    link: {
+      github: "https://github.com/Innovavtion/kanban-board",
+      vercel: "https://kanban-board-e2sm.vercel.app/",
+    },
+  },
+  {
+    name: "Weather",
+    description:
+      "Маленькое приложение для просмотра текущей погоды в нескольких городах.",
+    image: WeatherImage,
+    stack: [
+      { name: "TypeScript", icon: <SiTypescript /> },
+      { name: "React", icon: <SiReact /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+    ],
+    link: {
+      github: "https://github.com/Innovavtion/weather-react",
+      vercel: "https://weather-react-two-sigma.vercel.app/",
+    },
+  },
+  {
+    name: "To-Do",
+    description:
+      "Приложение для планирования своих задач на день или больше. Был расширен базовый функционал списка задач, для большего удобства использования приложения.",
+    stack: [
+      { name: "JavaScript", icon: <SiJavascript /> },
+      { name: "React", icon: <SiReact /> },
+      { name: "Sass", icon: <IoLogoSass /> },
+    ],
+    image: ToDoImage,
+    link: {
+      github: "https://github.com/Innovavtion/to-do-react",
+      vercel: "https://to-do-react-first-project.vercel.app/",
+    },
+  },
+];
--- components/Section/Portfolio/Portfolio.tsx
+++ components/Section/Portfolio/Portfolio.tsx
@@ -1,147 +1,60 @@
+"use client";
+
 import styles from "./Portfolio.module.scss";
 
 import Image from "next/image";
-import KanbanImage from "@/public/Images/Kanban.png";
+import Link from "next/link";
 
 import { Button } from "@/components/Elements/Base/Button/Button";
-
-import { SiTypescript, SiNextdotjs, SiGithub, SiMui } from "react-icons/si";
+import { SiGithub } from "react-icons/si";
 import { FiExternalLink } from "react-icons/fi";
+
+import { portfolioData } from "./Portfolio.data";
 
 export default function Portfolio() {
   return (
     <div className={styles.Portfolio}>
       <div className={styles.Container}>
         <h2>Portfolio</h2>
-        <div className={styles.Content}>
-          <div className={styles.PortfolioImage}>
-            <Image src={KanbanImage} alt="Kanban" className={styles.Image} />
-          </div>
-          <div className={styles.Card}>
-            <div className={styles.CardText}>
-              <div className={styles.Name}>
-                <h5>Kanban</h5>
-              </div>
-              <div className={styles.Description}>
-                <p>
-                  Задача организации, в особенности же рамки и место обучения
-                  кадров обеспечивает широкому кругу (специалистов) участие в
-                  формировании дальнейших направлений развития.
-                </p>
-              </div>
+        {portfolioData.map((item, key) => (
+          <div className={styles.Content} key={key}>
+            <div className={styles.PortfolioImage}>
+              <Image src={item.image} alt="Kanban" className={styles.Image} />
             </div>
-            <div className={styles.CardButton}>
-              <div className={styles.Stack}>
-                <Button>
-                  <SiTypescript />
-                  <p>TypeScript</p>
-                </Button>
-                <Button>
-                  <SiNextdotjs />
-                  <p>Next</p>
-                </Button>
-                <Button>
-                  <SiMui />
-                  <p>MUI</p>
-                </Button>
+            <div className={styles.Card}>
+              <div className={styles.CardText}>
+                <div className={styles.Name}>
+                  <h5>{item.name}</h5>
+                </div>
+                <div className={styles.Description}>
+                  <p>{item.description}</p>
+                </div>
               </div>
-              <div className={styles.View}>
-                <Button>
-                  <SiGithub />
-                </Button>
-                <Button>
-                  <FiExternalLink />
-                </Button>
+              <div className={styles.CardButton}>
+                <div className={styles.Stack}>
+                  {item.stack.map((itemStack, key) => (
+                    <Button key={key}>
+                      {itemStack.icon}
+                      <p>{itemStack.name}</p>
+                    </Button>
+                  ))}
+                </div>
+                <div className={styles.View}>
+                  <Link href={item.link.github} target="blank">
+                    <Button>
+                      <SiGithub />
+                    </Button>
+                  </Link>
+                  <Link href={item.link.vercel} target="blank">
+                    <Button>
+                      <FiExternalLink />
+                    </Button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={styles.Content}>
-          <div className={styles.PortfolioImage}>
-            <Image src={KanbanImage} alt="Kanban" className={styles.Image} />
-          </div>
-          <div className={styles.Card}>
-            <div className={styles.CardText}>
-              <div className={styles.Name}>
-                <h5>Kanban</h5>
-              </div>
-              <div className={styles.Description}>
-                <p>
-                  Задача организации, в особенности же рамки и место обучения
-                  кадров обеспечивает широкому кругу (специалистов) участие в
-                  формировании дальнейших направлений развития.
-                </p>
-              </div>
-            </div>
-            <div className={styles.CardButton}>
-              <div className={styles.Stack}>
-                <Button>
-                  <SiTypescript />
-                  <p>TypeScript</p>
-                </Button>
-                <Button>
-                  <SiNextdotjs />
-                  <p>Next</p>
-                </Button>
-                <Button>
-                  <SiMui />
-                  <p>MUI</p>
-                </Button>
-              </div>
-              <div className={styles.View}>
-                <Button>
-                  <SiGithub />
-                </Button>
-                <Button>
-                  <FiExternalLink />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.Content}>
-          <div className={styles.PortfolioImage}>
-            <Image src={KanbanImage} alt="Kanban" className={styles.Image} />
-          </div>
-          <div className={styles.Card}>
-            <div className={styles.CardText}>
-              <div className={styles.Name}>
-                <h5>Kanban</h5>
-              </div>
-              <div className={styles.Description}>
-                <p>
-                  Задача организации, в особенности же рамки и место обучения
-                  кадров обеспечивает широкому кругу (специалистов) участие в
-                  формировании дальнейших направлений развития.
-                </p>
-              </div>
-            </div>
-            <div className={styles.CardButton}>
-              <div className={styles.Stack}>
-                <Button>
-                  <SiTypescript />
-                  <p>TypeScript</p>
-                </Button>
-                <Button>
-                  <SiNextdotjs />
-                  <p>Next</p>
-                </Button>
-                <Button>
-                  <SiMui />
-                  <p>MUI</p>
-                </Button>
-              </div>
-              <div className={styles.View}>
-                <Button>
-                  <SiGithub />
-                </Button>
-                <Button>
-                  <FiExternalLink />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
